78c15c8893458f16da5788d76b82e0806c9495c2
aragami3070 <smirnov17612@gmail.com>
2025-08-15 23:21:53 +0400

    Feat(three-activities): add task solution

--- round-916/three-activities/src/main.rs
+++ round-916/three-activities/src/main.rs
@@ -107,4 +107,82 @@ impl Triple {
 use std::{io, mem::swap};
 
 fn main() {
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).expect("Failed input");
+
+    let t: u16 = input.trim().parse().expect("t must be a number");
+
+    for _ in 0..t {
+        input.clear();
+        io::stdin().read_line(&mut input).expect("Failed input");
+
+        let _n: u32 = input.trim().parse().expect("n must be a number");
+
+        let mut max_ski = Triple::new();
+        let mut max_film = Triple::new();
+        let mut max_game = Triple::new();
+
+        input.clear();
+        io::stdin().read_line(&mut input).expect("Failed input");
+
+        for (index, s) in input.split_whitespace().enumerate() {
+            let num = s.parse::<u32>().expect("parse error");
+            if num > max_ski.third.0
+                || num > max_ski.second.0
+                || num > max_ski.first.0
+                || !max_ski.is_full()
+            {
+                max_ski.add((num, index as u16));
+            };
+        }
+
+        input.clear();
+        io::stdin().read_line(&mut input).expect("Failed input");
+
+        for (index, s) in input.split_whitespace().enumerate() {
+            let num = s.parse::<u32>().expect("parse error");
+            if num > max_film.third.0
+                || num > max_film.second.0
+                || num > max_film.first.0
+                || !max_film.is_full()
+            {
+                max_film.add((num, index as u16));
+            };
+        }
+
+        input.clear();
+        io::stdin().read_line(&mut input).expect("Failed input");
+
+        for (index, s) in input.split_whitespace().enumerate() {
+            let num = s.parse::<u32>().expect("parse error");
+            if num > max_game.third.0
+                || num > max_game.second.0
+                || num > max_game.first.0
+                || !max_game.is_full()
+            {
+                max_game.add((num, index as u16));
+            };
+        }
+
+        let ski = vec![max_ski.first, max_ski.second, max_ski.third];
+        let film = vec![max_film.first, max_film.second, max_film.third];
+        let game = vec![max_game.first, max_game.second, max_game.third];
+
+        let mut result = 0;
+
+        for s in ski {
+            for f in &film {
+                for g in &game {
+                    if s.1 != f.1 && f.1 != g.1 && g.1 != s.1 {
+                        let sum = s.0 + f.0 + g.0;
+                        if sum > result {
+                            result = sum
+                        }
+                    }
+                }
+            }
+        }
+        println!("{result}")
+    }
 }
