41251865a5e4fef9f1fb829c6d4ab8a5aa298ba3
aragami3070 <smirnov17612@gmail.com>
2025-08-24 23:04:17 +0400

    Feat(villagers): add task solution
    Round 1044

---
+++ round-1044/villagers/src/main.rs
@@ -0,0 +1,29 @@
+use std::io;
+fn main() {
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).expect("Failed input");
+
+    let t: u16 = input.trim().parse().expect("t should be a u16");
+
+    for _ in 0..t {
+        input.clear();
+        io::stdin().read_line(&mut input).expect("Failed input");
+        let _n: u32 = input.trim().parse().expect("n shoold be a u32");
+
+        input.clear();
+        io::stdin().read_line(&mut input).expect("Failed input");
+
+        let mut people: Vec<u64> = input
+            .split_whitespace()
+            .map(|num| num.trim().parse::<u64>().expect("Failed parse"))
+            .collect();
+        people.sort();
+
+        let mut sum = 0;
+        for i in (0..people.len()).rev().step_by(2) {
+            sum += people[i];
+        }
+        println!("{sum}")
+    }
+}
