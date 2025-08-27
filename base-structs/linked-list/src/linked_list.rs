140ca60af047a946c5ab24a14bebf6069dc918fa
aragami3070 <smirnov17612@gmail.com>
2025-08-27 16:42:27 +0400

    Feat(linked-list): add LinkedList<T> struct with impl method new

--- base-structs/linked-list/src/linked_list.rs
+++ base-structs/linked-list/src/linked_list.rs
@@ -4,9 +4,19 @@ struct Node<T> {
     next: Option<Box<Node<T>>>,
 }
 
+#[derive(Debug)]
+pub struct LinkedList<T> {
+    head: Option<Box<Node<T>>>,
+}
 
 impl<T> Node<T> {
     pub fn new(value: T) -> Node<T> {
         Node { value, next: None }
+    }
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> LinkedList<T> {
+        LinkedList { head: None }
     }
 }
